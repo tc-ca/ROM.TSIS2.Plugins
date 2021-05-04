6497a86bf6a3456ba3eaeb53dc21a0c4c49084ab
Jason Au <jason@infusionws.com>
2021-05-04 13:11:05 -0400

    sharing test context where possible

--- ROMTS-GSRST.Plugins.Tests/PreOperationmsdyn_workorderservicetaskCreateTests.cs
+++ ROMTS-GSRST.Plugins.Tests/PreOperationmsdyn_workorderservicetaskCreateTests.cs
@@ -12,79 +12,61 @@ namespace ROMTS_GSRST.Plugins.Tests
 {
     public class PreOperationmsdyn_workorderservicetaskCreateTests : UnitTestBase
     {
-        public PreOperationmsdyn_workorderservicetaskCreateTests(XrmMockupFixture fixture) : base(fixture) { }
+
+        Guid _regulatedEntityId;
+        Guid _workOrderId;
+
+        public PreOperationmsdyn_workorderservicetaskCreateTests(XrmMockupFixture fixture) : base(fixture) 
+        {
+            _regulatedEntityId = orgAdminUIService.Create(new Account() { Name = "Test Regulated Entity" });
+            _workOrderId = orgAdminUIService.Create(new msdyn_workorder()
+            {
+                msdyn_name = "300-345678",
+                msdyn_ServiceRequest = null,
+                ovs_regulatedentity = new EntityReference(Account.EntityLogicalName, _regulatedEntityId)
+
+            });
+        }
 
         [Fact]
         public void When_parent_workorder_has_name_expect_child_service_task_with_prefix_equal_to_200_parent_name()
         {
-            using (var context = new Xrm(orgAdminUIService))
+            // ACT
+            var workOrderServiceTaskId = orgAdminUIService.Create(new msdyn_workorderservicetask()
             {
-                /**********
-                * ARRANGE
-                **********/
-                var regulatedEntityId = orgAdminUIService.Create(new Account() { Name = "Test Regulated Entity" });
-                var workOrderId = orgAdminUIService.Create(new msdyn_workorder()
-                {
-                    msdyn_name = "300-34567",
-                    msdyn_ServiceRequest = null,
-                    ovs_regulatedentity = new EntityReference(Account.EntityLogicalName, regulatedEntityId)
+                msdyn_name = "SATR Boarding Gate Inspection", // sample name based off service task name, it should change after creation
+                msdyn_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, _workOrderId), // belongs to a work order
+            });
 
-                });
+            // ASSERT
+            var workOrderServiceTask = orgAdminUIService.Retrieve(msdyn_workorderservicetask.EntityLogicalName, workOrderServiceTaskId, new ColumnSet("msdyn_name")).ToEntity<msdyn_workorderservicetask>();
 
-                /**********
-                * ACT
-                **********/
-                var workOrderServiceTaskId = orgAdminUIService.Create(new msdyn_workorderservicetask()
-                {
-                    msdyn_name = "SATR Boarding Gate Inspection", // sample name based off service task name, it should change after creation
-                    msdyn_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, workOrderId), // belongs to a work order
-                });
-
-                /**********
-                 * ASSERT
-                 **********/
-                var workOrderServiceTask = orgAdminUIService.Retrieve(msdyn_workorderservicetask.EntityLogicalName, workOrderServiceTaskId, new ColumnSet("msdyn_name")).ToEntity<msdyn_workorderservicetask>();
-
-                // Expect work order service task name to start with parent work order's name but prefixed with 200-
-                Assert.StartsWith("200-34567", workOrderServiceTask.msdyn_name);
-            }
+            // Expect work order service task name to start with parent work order's name but prefixed with 200-
+            Assert.StartsWith("200-345678", workOrderServiceTask.msdyn_name);
         }
 
         [Fact]
         public void When_parent_workorder_has_1_previous_service_tasks_expect_new_service_task_with_suffix_2()
         {
-            /**********
-            * ARRANGE
-            **********/
-            var regulatedEntityId = orgAdminUIService.Create(new Account() { Name = "Test Regulated Entity" });
-            var workOrderId = orgAdminUIService.Create(new msdyn_workorder()
-            {
-                msdyn_name = "300-34567",
-                msdyn_ServiceRequest = null,
-                ovs_regulatedentity = new EntityReference(Account.EntityLogicalName, regulatedEntityId)
-            });
+            // ARRANGE
             var existingWorkOrderServiceTaskId = orgAdminUIService.Create(new msdyn_workorderservicetask()
             {
                 msdyn_name = "200-34567-1",
-                msdyn_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, workOrderId)
+                msdyn_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, _workOrderId)
             });
 
-            /**********
-            * ACT
-            **********/
+            // ACT
             var targetWorkOrderServiceTaskId = orgAdminUIService.Create(new msdyn_workorderservicetask()
             {
                 msdyn_name = "SATR Boarding Gate Inspection", // sample name based off service task name, it should change after creation
-                msdyn_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, workOrderId), // belongs to a work order
+                msdyn_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, _workOrderId), // belongs to a work order
             });
 
-            /**********
-            * ASSERT
-            **********/
+            // ASSERT
             var targetWorkOrderServiceTask = orgAdminUIService.Retrieve(msdyn_workorderservicetask.EntityLogicalName, targetWorkOrderServiceTaskId, new ColumnSet("msdyn_name")).ToEntity<msdyn_workorderservicetask>();
 
             // Expect work order service task name to start with parent work order's name and suffixed with -2
-            Assert.Equal("200-34567-2", targetWorkOrderServiceTask.msdyn_name);
+            Assert.Equal("200-345678-2", targetWorkOrderServiceTask.msdyn_name);
 
         }
     }
