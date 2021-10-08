10994cc98455bbffe46db14810e7ffe71721034f
DBurneyTC <Derek.Burney@tc.gc.ca>
2021-10-08 12:13:29 -0400

    Maintain data integrity test

--- ROMTS-GSRST.Plugins.Tests/PreOperationmsdyn_workorderUpdateTests.cs
+++ ROMTS-GSRST.Plugins.Tests/PreOperationmsdyn_workorderUpdateTests.cs
@@ -41,7 +41,7 @@ namespace ROMTS_GSRST.Plugins.Tests
             Assert.Equal(msdyn_wosystemstatus.ClosedPosted, workOrder.msdyn_SystemStatus);
         }
         [Fact]
-        public void When_case_of_work_order_updated_to_new_case_existing_findings_of_old_case_associate_to_new_case()
+        public void When_case_of_work_order_updated_to_new_case_expect_existing_findings_of_old_case_to_be_associated_to_new_case()
         {
             //ARRANGE
             var case1Id = orgAdminUIService.Create(new Incident { });
@@ -70,7 +70,7 @@ namespace ROMTS_GSRST.Plugins.Tests
         }
 
         [Fact]
-        public void When_case_of_work_order_case_is_set_to_null_expect_case_of_all_findings_to_also_be_set_to_null()
+        public void When_case_of_work_order_is_set_to_null_expect_case_of_all_findings_to_also_be_set_to_null()
         {
             //ARRANGE
             var case1Id = orgAdminUIService.Create(new Incident { });
@@ -96,6 +96,51 @@ namespace ROMTS_GSRST.Plugins.Tests
             Assert.Null(findings[0].ovs_CaseId);
             Assert.Null(findings[1].ovs_CaseId);
             Assert.Null(findings[2].ovs_CaseId);
+        }
+
+        [Fact]
+        public void When_case_of_work_order_is_changed_expect_all_findings_not_associated_to_work_order_to_remain_unchanged()
+        {
+            //ARRANGE
+            var case1Id = orgAdminUIService.Create(new Incident { });
+            var case2Id = orgAdminUIService.Create(new Incident { });
+            var case3Id = orgAdminUIService.Create(new Incident { });
+
+            var workOrder1Id = orgAdminService.Create(new msdyn_workorder { msdyn_ServiceRequest = new EntityReference(Incident.EntityLogicalName, case1Id) });
+            var workOrder2Id = orgAdminService.Create(new msdyn_workorder { msdyn_ServiceRequest = new EntityReference(Incident.EntityLogicalName, case1Id) });
+
+            //Make 3 findings related to case1 and workOrder1
+            var finding1 = orgAdminService.Create(new ovs_Finding { ovs_CaseId = new EntityReference(Incident.EntityLogicalName, case1Id), ts_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, workOrder1Id) });
+            var finding2 = orgAdminService.Create(new ovs_Finding { ovs_CaseId = new EntityReference(Incident.EntityLogicalName, case1Id), ts_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, workOrder1Id) });
+            var finding3 = orgAdminService.Create(new ovs_Finding { ovs_CaseId = new EntityReference(Incident.EntityLogicalName, case1Id), ts_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, workOrder1Id) });
+
+            //Make 3 findings related to case2 and workOrder2
+            var finding4 = orgAdminService.Create(new ovs_Finding { ovs_CaseId = new EntityReference(Incident.EntityLogicalName, case2Id), ts_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, workOrder2Id) });
+            var finding5 = orgAdminService.Create(new ovs_Finding { ovs_CaseId = new EntityReference(Incident.EntityLogicalName, case2Id), ts_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, workOrder2Id) });
+            var finding6 = orgAdminService.Create(new ovs_Finding { ovs_CaseId = new EntityReference(Incident.EntityLogicalName, case2Id), ts_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, workOrder2Id) });
+
+            //ACT
+            //Update workOrder2 to be related to case3
+            //All findings related to workOrder2 should then be updated to be related to case3
+            orgAdminService.Update(new msdyn_workorder { Id = workOrder2Id, msdyn_ServiceRequest = new EntityReference(Incident.EntityLogicalName, case3Id) });
+
+            //ASSERT
+            var query = new QueryExpression(ovs_Finding.EntityLogicalName)
+            {
+                ColumnSet = new ColumnSet("ovs_caseid")
+            };
+            var case1findings = orgAdminUIService.RetrieveMultiple(query).Entities.Cast<ovs_Finding>().Where(f => f.ovs_CaseId.Id == case1Id).ToList();
+            var case2findings = orgAdminUIService.RetrieveMultiple(query).Entities.Cast<ovs_Finding>().Where(f => f.ovs_CaseId.Id == case2Id).ToList();
+            var case3findings = orgAdminUIService.RetrieveMultiple(query).Entities.Cast<ovs_Finding>().Where(f => f.ovs_CaseId.Id == case3Id).ToList();
+
+            //Expect 3 findings to be related to case 1
+            Assert.Equal(3, case1findings.Count);
+
+            //Expect 0 findings to be related to case 2
+            Assert.Empty(case2findings);
+
+            //Expect 3 findings to be related to case 3
+            Assert.Equal(3, case3findings.Count);
         }
     }
 }
