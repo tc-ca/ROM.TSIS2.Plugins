fe8cc5fef427b4e73a47cfad261ca168e3a11346
Petrie, Robert <robert.petrie@tc.gc.ca>
2022-02-22 14:56:38 -0500

    Code for if a Work Order Service Task has been moved to another Work Order, update the associated files with the new Work Order and Case

---
+++ TSIS2.Plugins/PostOperationmsdyn_workorderservicetaskUpdate.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSIS2.Plugins
+{
+    [CrmPluginRegistration(
+    MessageNameEnum.Update,
+    "msdyn_workorderservicetask",
+    StageEnum.PostOperation,
+    ExecutionModeEnum.Synchronous,
+    "msdyn_workorder",
+    "TSIS2.Plugins.PostOperationmsdyn_workorderservicetaskUpdate Plugin",
+    1,
+    IsolationModeEnum.Sandbox,
+    Image1Name = "PostImage", Image1Type = ImageTypeEnum.PostImage, Image1Attributes = "msdyn_name,msdyn_workorder",
+    Description = "If a Work Order Service Task has been moved to another Work Order, update the associated files with the new Work Order and Case")]
+    /// <summary>
+    /// PostOperationmsdyn_workorderservicetaskUpdate Plugin.
+    /// </summary>  
+    public class PostOperationmsdyn_workorderservicetaskUpdate : PluginBase
+    {
+        private readonly string postImageAlias = "PostImage";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PostOperationmsdyn_workorderservicetaskUpdate"/> class.
+        /// </summary>
+        /// <param name="unsecure">Contains public (unsecured) configuration information.</param>
+        /// <param name="secure">Contains non-public (secured) configuration information. 
+        /// When using Microsoft Dynamics 365 for Outlook with Offline Access, 
+        /// the secure string is not passed to a plug-in that executes while the client is offline.</param>
+        public PostOperationmsdyn_workorderservicetaskUpdate(string unsecure, string secure)
+            : base(typeof(PostOperationts_workorderactivitytypeUpdate))
+        {
+            //if (secure != null &&!secure.Equals(string.Empty))
+            //{
+
+            //}
+        }
+
+        /// <summary>
+        /// Main entry point for he business logic that the plug-in is to execute.
+        /// </summary>
+        /// <param name="localContext">The <see cref="LocalPluginContext"/> which contains the
+        /// <see cref="IPluginExecutionContext"/>,
+        /// <see cref="IOrganizationService"/>
+        /// and <see cref="ITracingService"/>
+        /// </param>
+        /// <remarks>
+        /// For improved performance, Microsoft Dynamics 365 caches plug-in instances.
+        /// The plug-in's Execute method should be written to be stateless as the constructor
+        /// is not called for every invocation of the plug-in. Also, multiple system threads
+        /// could execute the plug-in at the same time. All per invocation state information
+        /// is stored in the context. This means that you should not use global variables in plug-ins.
+        /// </remarks>
+        protected override void ExecuteCrmPlugin(LocalPluginContext localContext)
+        {
+            if (localContext == null)
+            {
+                throw new InvalidPluginExecutionException("localContext");
+            }
+
+            IPluginExecutionContext context = localContext.PluginExecutionContext;
+            Entity target = (Entity)context.InputParameters["Target"];
+            Entity postImageEntity = (context.PostEntityImages != null && context.PostEntityImages.Contains(this.postImageAlias)) ? context.PostEntityImages[this.postImageAlias] : null;
+
+            try
+            {
+                if (target.LogicalName.Equals(msdyn_workorderservicetask.EntityLogicalName))
+                {
+                    // If Work Order is Updated - Update any associated files with the new Work Order and Case
+                    {
+                        if (target.Attributes.Contains("msdyn_workorder"))
+                        {
+                            using (var serviceContext = new Xrm(localContext.OrganizationService))
+                            {
+                                // Cast the target to the expected entity
+                                msdyn_workorderservicetask myWorkOrderServiceTask = target.ToEntity<msdyn_workorderservicetask>();
+
+                                // Get the selected Work Order Service Task
+                                var selectedWorkOrderServiceTask = serviceContext.msdyn_workorderservicetaskSet.Where(wost => wost.Id == myWorkOrderServiceTask.Id).FirstOrDefault();
+
+                                // Get the Work Order associated with the Work Order Service Task
+                                var selectedWorkOrder = serviceContext.msdyn_workorderSet.Where(wo => wo.Id == selectedWorkOrderServiceTask.msdyn_WorkOrder.Id).FirstOrDefault();
+
+                                // Retrieve all the files that are associated with the Work Order Service Task
+                                var allFiles = serviceContext.ts_FileSet.ToList();
+                                var workOrderServiceTaskFiles = allFiles.Where(f => f.ts_formintegrationid != null && f.ts_formintegrationid.Replace("WOST ", "").Trim() == selectedWorkOrderServiceTask.msdyn_name).ToList();
+
+                                if (workOrderServiceTaskFiles != null)
+                                {
+                                    foreach (var file in workOrderServiceTaskFiles)
+                                    {
+                                        localContext.OrganizationService.Update(new ts_File
+                                        {
+                                            Id = file.Id,
+                                            ts_msdyn_workorder = selectedWorkOrder.ToEntityReference(),
+                                            ts_Incident = selectedWorkOrder.msdyn_ServiceRequest
+                                        });
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new InvalidPluginExecutionException(e.Message);
+            }
+        }
+    }
+}
