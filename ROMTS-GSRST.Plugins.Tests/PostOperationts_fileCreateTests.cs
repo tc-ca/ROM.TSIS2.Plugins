43f86d66516ffb8a754185c16096634b7089291c
Petrie, Robert <robert.petrie@tc.gc.ca>
2022-02-23 11:22:24 -0500

    Added and changed the names of test cases for PostOperationts_fileCreateTests

--- ROMTS-GSRST.Plugins.Tests/PostOperationts_fileCreateTests.cs
+++ ROMTS-GSRST.Plugins.Tests/PostOperationts_fileCreateTests.cs
@@ -42,7 +42,7 @@ namespace ROMTS_GSRST.Plugins.Tests
         }
 
         [Fact]
-        public void Create_test_file_with_no_work_order_service_task()
+        public void Create_test_file_with_no_work_order_in_work_order_service_task()
         {
             using (var context = new Xrm(orgAdminService))
             {
@@ -84,7 +84,7 @@ namespace ROMTS_GSRST.Plugins.Tests
         }
 
         [Fact]
-        public void Create_test_file_with_work_order_service_task()
+        public void Create_test_file_with_case_and_work_order_from_work_order_service_task()
         {
             using (var context = new Xrm(orgAdminService))
             {
@@ -124,6 +124,120 @@ namespace ROMTS_GSRST.Plugins.Tests
 
                 // Expect the file to be associated with Case 'My test case'
                 Assert.Equal("My test case", result.ts_Incident.Name);
+            }
+        }
+
+        [Fact]
+        public void Create_test_file_with_no_case_and_work_order_from_work_order_service_task()
+        {
+            using (var context = new Xrm(orgAdminService))
+            {
+                // ARRANGE
+                var file = new ts_File();
+
+                var workOrderId = orgAdminService.Create(new msdyn_workorder()
+                {
+                    msdyn_name = "1234"
+                });
+
+                var workOrderServiceTaskId = orgAdminService.Create(new msdyn_workorderservicetask()
+                {
+                    msdyn_name = "1234",
+                    msdyn_WorkOrder = new EntityReference(msdyn_workorder.EntityLogicalName, workOrderId)
+                });
+
+                // ACT
+                file.Id = orgAdminService.Create(new ts_File()
+                {
+                    ts_File_1 = "File test.txt",
+                    ts_FileContext = ts_filecontext.TC2020PROMOTINGCOMPLIANCE,
+                    ts_formintegrationid = "WOST 1234-1",
+                    ts_VisibletoOtherPrograms = false,
+                });
+
+                // ASSERT
+                var result = orgAdminService.Retrieve(ts_File.EntityLogicalName, file.Id, new ColumnSet(true)).ToEntity<ts_File>();
+
+                // Expect the file to be associated with Work Order 1234
+                Assert.Equal("1234", result.ts_msdyn_workorder.Name);
+
+                // Expect the file to not be associated with a Case
+                Assert.Null(result.ts_Incident);
+            }
+        }
+
+        [Fact]
+        public void Create_test_file_with_case_from_work_order()
+        {
+            using (var context = new Xrm(orgAdminService))
+            {
+                // ARRANGE
+                var file = new ts_File();
+                var myCaseId = orgAdminService.Create(new Incident()
+                {
+                    Title = "My test case"
+                });
+
+                var workOrderId = orgAdminService.Create(new msdyn_workorder()
+                {
+                    msdyn_name = "1234",
+                    msdyn_ServiceRequest = new EntityReference(Incident.EntityLogicalName, myCaseId)
+                });
+
+                // ACT
+                file.Id = orgAdminService.Create(new ts_File()
+                {
+                    ts_File_1 = "File test.txt",
+                    ts_FileContext = ts_filecontext.TC2020PROMOTINGCOMPLIANCE,
+                    ts_formintegrationid = "WO 1234",
+                    ts_VisibletoOtherPrograms = false,
+                });
+
+                // ASSERT
+                var result = orgAdminService.Retrieve(ts_File.EntityLogicalName, file.Id, new ColumnSet(true)).ToEntity<ts_File>();
+
+                // Expect the file to not be associated with a Work Order
+                // Note: Remember files coming in from Document Centre - Work Order do not need to have the ts_msdyn_workorder populated,
+                // only files coming in from Document Centre - Work Order Service Task use that.
+                Assert.Null(result.ts_msdyn_workorder);
+
+                // Expect the file to be associated with Case 'My test case'
+                Assert.Equal("My test case", result.ts_Incident.Name);
+            }
+        }
+
+        [Fact]
+        public void Create_test_file_with_no_case_from_work_order()
+        {
+            using (var context = new Xrm(orgAdminService))
+            {
+                // ARRANGE
+                var file = new ts_File();
+
+                var workOrderId = orgAdminService.Create(new msdyn_workorder()
+                {
+                    msdyn_name = "1234"
+                });
+
+                // ACT
+                file.Id = orgAdminService.Create(new ts_File()
+                {
+                    ts_File_1 = "File test.txt",
+                    ts_FileContext = ts_filecontext.TC2020PROMOTINGCOMPLIANCE,
+                    ts_formintegrationid = "WO 1234",
+                    ts_VisibletoOtherPrograms = false,
+                });
+
+                // ASSERT
+                var result = orgAdminService.Retrieve(ts_File.EntityLogicalName, file.Id, new ColumnSet(true)).ToEntity<ts_File>();
+
+                // Expect the file to not be associated with a Work Order
+                // Note: Remember files coming in from Document Centre - Work Order do not need to have the ts_msdyn_workorder populated,
+                // only files coming in from Document Centre - Work Order Service Task use that.
+                Assert.Null(result.ts_msdyn_workorder);
+
+                // Expect the file to not be associated with a Case
+                Assert.Null(result.ts_Incident);
             }
         }
     }
