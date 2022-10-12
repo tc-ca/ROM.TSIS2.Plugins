24261afb78900a239d3f293502daa8d56ded7b10
DBurneyTC <Derek.Burney@tc.gc.ca>
2022-10-12 09:28:27 -0400

    XrmContext and empty teamplanningdataCreate plugin

---
+++ TSIS2.Plugins/PostOperationts_teamplanningdataCreate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+
+namespace TSIS2.Plugins
+{
+
+    [CrmPluginRegistration(
+        MessageNameEnum.Create,
+        "ts_teamplanningdata",
+        StageEnum.PostOperation,
+        ExecutionModeEnum.Synchronous,
+        "",
+        "TSIS2.Plugins.PostOperationts_teamplanningdataCreate Plugin",
+        1,
+        IsolationModeEnum.Sandbox,
+        Description = "After Team Planning Data is created, automatically create Planning Data")]
+    public class PostOperationts_teamplanningdataCreate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the tracing service
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.
+                Entity target = (Entity)context.InputParameters["Target"];
+
+                // Obtain the preimage entity
+                Entity preImageEntity = (context.PreEntityImages != null && context.PreEntityImages.Contains("PreImage")) ? context.PreEntityImages["PreImage"] : null;
+
+                // Obtain the organization service reference which you will need for
+                // web service calls.
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+                    if (target.LogicalName.Equals(ts_TeamPlanningData.EntityLogicalName))
+                    {
+                        ts_TeamPlanningData teamPlanningData = target.ToEntity<ts_TeamPlanningData>();
+
+                        
+                    }
+                }
+                catch (NotImplementedException ex)
+                {
+                    // If exceptions from mocking library, just continue. If not from there, we should still throw the error.
+                    if (ex.Source == "XrmMockup365" && ex.Message == "No implementation for expression operator 'Count'")
+                    {
+                        // continue
+                    }
+                    else
+                    {
+                        tracingService.Trace("PostOperationts_teamplanningdataCreate Plugin: {0}", ex.ToString());
+                        throw;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    tracingService.Trace("PostOperationts_teamplanningdataCreate Plugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
