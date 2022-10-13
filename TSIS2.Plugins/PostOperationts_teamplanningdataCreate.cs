47b2c220a3ef241279bd2e0779aaa954b269451a
DBurneyTC <Derek.Burney@tc.gc.ca>
2022-10-13 19:43:03 -0400

    Quarter functions. determine next expected inspection quarter

--- TSIS2.Plugins/PostOperationts_teamplanningdataCreate.cs
+++ TSIS2.Plugins/PostOperationts_teamplanningdataCreate.cs
@@ -66,25 +66,47 @@ namespace TSIS2.Plugins
                                     msdyn_incidenttype incidentType = serviceContext.msdyn_incidenttypeSet.FirstOrDefault(it => it.Id == operationActivity.ts_Activity.Id);
                                     ts_RecurrenceFrequencies recurrenceFrequency = serviceContext.ts_RecurrenceFrequenciesSet.FirstOrDefault(rf => rf.Id == incidentType.ts_RiskScore.Id);
                                     msdyn_FunctionalLocation functionalLocation = serviceContext.msdyn_FunctionalLocationSet.FirstOrDefault(fl => fl.Id == operation.ts_site.Id);
+                                    tc_TCFiscalYear fiscalYear = serviceContext.tc_TCFiscalYearSet.FirstOrDefault(fy => fy.Id == targetTeamPlanngingData.ts_FiscalYear.Id);
 
                                     string englishName = operation.ovs_name + " | " + incidentType.ovs_IncidentTypeNameEnglish + " | " + teamPlanningData.ts_FiscalYear.Name;
                                     string frenchName = operation.ovs_name + " | " + incidentType.ovs_IncidentTypeNameFrench + " | " + teamPlanningData.ts_FiscalYear.Name;
 
+                                    var inspections = serviceContext.msdyn_workorderSet.Where(wo => wo.msdyn_PrimaryIncidentType.Id == incidentType.Id && wo.ovs_OperationId.Id == operation.Id);
+                                    tc_TCFiscalQuarter latestFiscalQuarter = GetLatestWorkOrderFiscalQuarter(inspections, serviceContext);
+
                                     int interval = 0;
-                                    int frequency = 0;
 
                                     if (recurrenceFrequency != null)
                                     {
-
+                                        if (functionalLocation.ts_Class == msdyn_FunctionalLocation_ts_Class._1)
+                                        {
+                                            interval = (int)recurrenceFrequency.ts_Class1Interval;
+                                        }
+                                        else //Class 2 or 3
+                                        {
+                                            if (functionalLocation.ts_RiskScore > 5)
+                                            {
+                                                interval = (int)recurrenceFrequency.ts_Class2and3HighRiskInterval;
+                                            } 
+                                            else
+                                            {
+                                                interval = (int)recurrenceFrequency.ts_Class2and3LowRiskInterval;
+                                            }
+                                        }
                                     }
-                                    if (functionalLocation.ts_Class == msdyn_FunctionalLocation_ts_Class._1)
-                                    {
-                                        interval = (int)recurrenceFrequency.ts_Class1Interval;
-                                        frequency = (int)recurrenceFrequency.ts_Class1Frequency;
-                                    } 
-                                    else //Class 2 or 3
-                                    {
+                                    int planningDataTarget = 0;
+                                    int planningDataDueQ1 = 0;
+                                    int planningDataDueQ2 = 0;
+                                    int planningDataDueQ3 = 0;
+                                    int planningDataDueQ4 = 0;
 
+                                    tc_TCFiscalQuarter nextExpectedInspectionQuarter = JumpQuarters(latestFiscalQuarter, interval, serviceContext);
+
+                                    //If the next expected inspection occurs before the current fiscal year, it's overdue and needs to occur Q1
+                                    if (((DateTime)nextExpectedInspectionQuarter.tc_QuarterStart).AddDays(1) <= fiscalYear.tc_FiscalStart)
+                                    {
+                                        planningDataTarget++;
+                                        planningDataDueQ1++;
                                     }
 
                                     service.Create(new ts_PlanningData
@@ -120,6 +142,38 @@ namespace TSIS2.Plugins
                     throw;
                 }
             }
+        }
+        //Returns to most recent fiscal quarter of a list of Work Orders
+        private tc_TCFiscalQuarter GetLatestWorkOrderFiscalQuarter(IQueryable<msdyn_workorder> workOrders, Xrm serviceContext)
+        {
+            msdyn_workorder latestWorkOrder = null;
+            tc_TCFiscalQuarter latestWorkOrderFiscalQuarter = null;
+            foreach(msdyn_workorder workOrder in workOrders)
+            {
+                if (latestWorkOrder == null)
+                {
+                    latestWorkOrder = workOrder;
+                    latestWorkOrderFiscalQuarter = serviceContext.tc_TCFiscalQuarterSet.FirstOrDefault(fc => fc.Id == workOrder.ovs_FiscalQuarter.Id);
+                } 
+                else 
+                {
+                    var currentWorkOrderFiscalQuarter = serviceContext.tc_TCFiscalQuarterSet.FirstOrDefault(fc => fc.Id == workOrder.ovs_FiscalQuarter.Id);
+                    if (currentWorkOrderFiscalQuarter.tc_QuarterEnd < latestWorkOrderFiscalQuarter.tc_QuarterEnd)
+                    {
+                        latestWorkOrder = workOrder;
+                        latestWorkOrderFiscalQuarter = currentWorkOrderFiscalQuarter;
+                    }
+                }
+            }
+            return latestWorkOrderFiscalQuarter;
+        }
+
+        //Return the Fiscal Quarter numberOfQuarters after the startingQuarter
+        private tc_TCFiscalQuarter JumpQuarters(tc_TCFiscalQuarter startingQuarter, int numberOfQuarters, Xrm serviceContext)
+        {
+            DateTime startingDate = (DateTime)startingQuarter.tc_QuarterEnd;
+            DateTime jumpDate = startingDate.AddMonths(numberOfQuarters * 3).AddDays(1);
+            return serviceContext.tc_TCFiscalQuarterSet.FirstOrDefault(fq => fq.tc_QuarterStart <= jumpDate && fq.tc_QuarterEnd >= jumpDate);
         }
     }
 }
