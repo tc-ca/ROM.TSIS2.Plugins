a059b77a4c5bec177c0fc46e6e10a98adae4f7f0
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2024-05-29 12:03:56 -0400

    Created PostOperation_AccountUpdate

---
+++ TSIS2.Plugins/PostOperation_AccountUpdate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSIS2.Plugins
+{
+    [CrmPluginRegistration(
+    MessageNameEnum.Update,
+    "account",
+    StageEnum.PostOperation,
+    ExecutionModeEnum.Synchronous,
+    "",
+    "TSIS2.Plugins.PostOperation_AccountUpdate Plugin",
+    1,
+    IsolationModeEnum.Sandbox,
+    Image1Name = "PostImage", Image1Type = ImageTypeEnum.PostImage, Image1Attributes = "",
+    Description = "If a stockholder is updated, the related operation names are being updated")]
+    /// <summary>
+    /// PostOperationmsdyn_workorderservicetaskUpdate Plugin.
+    /// </summary>  
+    public class PostOperation_AccountUpdate : PluginBase
+    {
+        private readonly string postImageAlias = "PostImage";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PostOperationmsdyn_workorderservicetaskUpdate"/> class.
+        /// </summary>
+        /// <param name="unsecure">Contains public (unsecured) configuration information.</param>
+        /// <param name="secure">Contains non-public (secured) configuration information. 
+        /// When using Microsoft Dynamics 365 for Outlook with Offline Access, 
+        /// the secure string is not passed to a plug-in that executes while the client is offline.</param>
+        public PostOperation_AccountUpdate(string unsecure, string secure)
+            : base(typeof(PostOperation_AccountUpdate))
+        {
+
+            //if (secure != null &&!secure.Equals(string.Empty))
+            //{
+
+            //}
+        }
+
+        protected override void ExecuteCrmPlugin(LocalPluginContext localContext)
+        {
+            if (localContext == null)
+            {
+                throw new InvalidPluginExecutionException("localContext");
+            }
+
+            try
+            { }
+            catch (Exception e) { throw new InvalidPluginExecutionException(e.Message); }
+        }
+
+    }
+}
